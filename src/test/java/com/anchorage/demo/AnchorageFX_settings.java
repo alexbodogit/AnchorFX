a478166d3c68c1f72790ea9a576eb067b74aa63b
Alessio Vinerbi <alexiphone@live.it>
2015-12-28 16:44:54 +0100

    Change title

--- src/test/java/com/anchorage/demo/AnchorageFX_settings.java
+++ src/test/java/com/anchorage/demo/AnchorageFX_settings.java
@@ -49,7 +49,7 @@ public class AnchorageFX_settings extends Application {
 
         AnchorageSystem.installDefaultStyle();
 
-        primaryStage.setTitle("Anchorage FX");
+        primaryStage.setTitle("AnchorFX");
         primaryStage.setScene(scene);
         primaryStage.show();
         
